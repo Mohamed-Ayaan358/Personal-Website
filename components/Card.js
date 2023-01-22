95a19dc096005b32b6bd4d4ab72fe515e7209929
Mohamed Ayaan <ayaan35813@gmail.com>
2023-01-22 23:42:03 +0530

    made prettier-1

--- components/Card.js
+++ components/Card.js
@@ -2,11 +2,11 @@ import Image from './Image'
 import Link from './Link'
 
 const Card = ({ title, description, imgSrc, href }) => (
-  <div className="md p-4 md:w-1/2" style={{ maxWidth: '544px' }}>
+  <div className="md p-4 md:w-1/2 " style={{ maxWidth: '544px' }}>
     <div
       className={`${
         imgSrc && 'h-full'
-      }  overflow-hidden rounded-md border-2 border-gray-200 border-opacity-60 dark:border-gray-700`}
+      }  overflow-hidden rounded-md border-gray-200 border-opacity-60 bg-cyan-300 dark:border-gray-700 dark:bg-gray-700`}
     >
       {imgSrc &&
         (href ? (
