d074643d5ab10136211c94ac87e9d32f7ba486a7
Mohamed Ayaan <ayaan35813@gmail.com>
2023-01-23 20:07:09 +0530

    Add pdf

--- pages/resume.js
+++ pages/resume.js
@@ -4,7 +4,8 @@ import Script from 'next/script'
 export default function Projects() {
   return (
     <>
-      <iframe src="./Ayaan.pdf" title="Async & Performance" width="1000" height="800" />
+      <p>Ayaan</p>
+      {/* <iframe src="./Ayaan.pdf" title="Async & Performance" width="1000" height="800" /> */}
     </>
   )
 }
