f2cd902fcdccaa202186e0b208119cfdb2f812f0
Mohamed Ayaan <ayaan35813@gmail.com>
2023-01-23 20:04:24 +0530

    Add pdf

---
+++ pages/resume.js
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+import Script from 'next/script'
+
+export default function Projects() {
+  return (
+    <>
+      <iframe src="./Ayaan.pdf" title="Async & Performance" width="1000" height="800" />
+    </>
+  )
+}
