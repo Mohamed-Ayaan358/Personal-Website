47725499fb29395e33ac63b6dfef50753233c8bd
Mohamed Ayaan <ayaan35813@gmail.com>
2023-10-24 19:21:50 +0530

    carousel

--- pages/projects.js
+++ pages/projects.js
@@ -18,7 +18,7 @@ export default function Projects() {
         </div>
         <div className="container py-12">
           <div className="-m-4 flex flex-wrap">
-            {projectsData.map((d) => (
+            {/* {projectsData.map((d) => (
               <Card
                 key={d.title}
                 title={d.title}
@@ -27,7 +27,81 @@ export default function Projects() {
                 href={d.href}
               />
               // Must modify the Card so it can be put css
-            ))}
+            ))} */}
+            <div>
+              <input className="carousel-input" id="one" name="multiples" type="radio" value="1" />
+              <label className="carousel-label" htmlFor="one">
+                1
+              </label>
+
+              <input className="carousel-input" id="two" name="multiples" type="radio" value="2" />
+              <label className="carousel-label" htmlFor="two">
+                2
+              </label>
+
+              <input
+                className="carousel-input"
+                id="three"
+                name="multiples"
+                type="radio"
+                value="3"
+              />
+              <label className="carousel-label" htmlFor="three">
+                3
+              </label>
+
+              <input className="carousel-input" id="four" name="multiples" type="radio" value="4" />
+              <label className="carousel-label" htmlFor="four">
+                4
+              </label>
+
+              <input className="carousel-input" id="five" name="multiples" type="radio" value="5" />
+              <label className="carousel-label" htmlFor="five">
+                5
+              </label>
+
+              <input className="carousel-input" id="six" name="multiples" type="radio" value="6" />
+              <label className="carousel-label" htmlFor="six">
+                6
+              </label>
+
+              <input
+                className="carousel-input"
+                id="seven"
+                name="multiples"
+                type="radio"
+                value="7"
+              />
+              <label className="carousel-label" htmlFor="seven">
+                7
+              </label>
+
+              <input
+                className="carousel-input"
+                id="eight"
+                name="multiples"
+                type="radio"
+                value="8"
+              />
+              <label className="carousel-label" htmlFor="eight">
+                8
+              </label>
+
+              <div className="container">
+                <div className="carousel">
+                  {projectsData.map((d) => (
+                    <Card
+                      key={d.title}
+                      title={d.title}
+                      description={d.description}
+                      imgSrc={d.imgSrc}
+                      href={d.href}
+                    />
+                    // Must modify the Card so it can be put css
+                  ))}
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
